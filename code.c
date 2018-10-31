b4fc597aa0c288e40868553ca5329416ec8b990d
obrutus <vaishnavani7@gmail.com>
2018-10-31 23:22:20 +0530

    next

--- code.c
+++ code.c
@@ -1,11 +1,19 @@
 #include<stdio.h>
 #include<math.h>
-int main()
+#include <sys/ioctl.h>
+
+int main (void,void)
 {
+    struct ttysize ts;
+    ioctl(0, TIOCGSIZE, &ts);
+
+    printf ("lines %d\n", ts.ts_lines);
+    printf ("columns %d\n", ts.ts_cols);
+	scanf("%d",&j);
 	int i,j;
 	while(1)
 	{
-		for(j=0;j<5;j++)
+		for(j=0;j<ts.ts_cols;j++)
 		{
 			i=rand()%100;
 			printf("%d\t",i);
